ff8554754b4d37a6415bb465d126618467ef375b
Manas-Agarwal-007 <manaagarwal@deloitte.com>
2023-10-06 12:23:05 +0530

    turning the debug mode off

--- nlp.py
+++ nlp.py
@@ -6,34 +6,26 @@ def convert_nlp_to_sql_poc(prompt_text):
     openai.api_key = "c4b435efc4a2442880527e59169438a8"
     openai.api_base = "https://cog-6sbix7titqeb2.openai.azure.com/"
     openai.api_type = 'azure'
-    openai.api_version = '2023-05-15'
-
+    openai.api_version = '2023-07-01-preview'
     deployment_name = 'davinci'
-
-    print('Based on the request, below is the reponse \n \n')
-
     database_name = "GenAI"
     serverless_connection_string = ('Driver={ODBC Driver 13 for SQL Server};Server=tcp:synw-infra-int-dev-ondemand.sql'
                                     '.azuresynapse.net,1433;Database=GenAI;Uid=masterdummy;Pwd={'
                                     '!pass@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-
     metadata_adv = generate_synapse_ddls(serverless_connection_string, database_name)
-
-    full_prompt = '''Given the following SQL tables, your job is to write the query given users question \n \n''' + str(
-        metadata_adv) + '''\n\n''' + str(prompt_text) + '''\n'''
-
+    full_prompt = '''Given the following SQL tables, ''' + str(
+        metadata_adv) + '''your job is to write the query given users question \n''' + str(prompt_text) + '''.'''
     response = openai.Completion.create(
         engine=deployment_name,
         prompt=full_prompt,
-        temperature=0.22,
+        temperature=0,
         max_tokens=800,
-        top_p=0.95,
+        top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
         stop=None)
-
     answer = response.choices[0].text.strip()
     return answer
 
 
-#print(convert_nlp_to_sql_poc("Write a SQL query which will give customers with highest amount of sales orders"))
+# print(convert_nlp_to_sql_poc("Write a SQL query which will give customers with highest amount of sales orders"))
