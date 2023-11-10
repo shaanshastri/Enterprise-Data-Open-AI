0e3cfa87b6b5772390e370a612f9550a5d5e20b7
shaanshastri <shashastri@deloitte.com.au>
2023-11-10 14:52:09 +1100

    Added metadata_ctlfwk to fetch metadta frm ctlffwk

--- nlp.py
+++ nlp.py
@@ -17,7 +17,7 @@ def convert_nlp_to_sql_poc(prompt_text):
     # deployment_name = 'davinci'
     # database_name = "GenAI_v2"
 
-    serverless_connection_string = ('Driver={ODBC Driver 18 for SQL Server};Server=tcp:synw-infra-int-dev-ondemand.sql'
+    serverless_connection_string = ('Driver={ODBC Driver 17 for SQL Server};Server=tcp:synw-infra-int-dev-ondemand.sql'
                                     '.azuresynapse.net,1433;Database=GenAI_v2;Uid=masterdummy_2;Pwd={'
                                     '!pass@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
     # metadata_adv = generate_synapse_ddls(serverless_connection_string, database_name)
@@ -52,7 +52,7 @@ def convert_nlp_to_sql_poc(prompt_text):
     my_pwd = "!pass@123"
     my_host = "tcp:synw-infra-int-dev-ondemand.sql.azuresynapse.net,1433"
     my_db = "GenAI_v2"
-    my_odbc_driver = "ODBC Driver 18 for SQL Server"
+    my_odbc_driver = "ODBC Driver 17 for SQL Server"
 
     connection_url = URL.create(
         "mssql+pyodbc",
@@ -73,12 +73,13 @@ def convert_nlp_to_sql_poc(prompt_text):
 
     #setting Azure OpenAI env variables
 
-    llm = AzureChatOpenAI(deployment_name="davinci", temperature=0, max_tokens=4000)
+    llm = AzureChatOpenAI(deployment_name="davinci", temperature=0, max_tokens=1000)
     # db_chain = SQLDatabaseChain.from_llm(llm=llm, db=db, verbose=True)
 
     # from langchain.chat_models import ChatOpenAI
     chain = create_sql_query_chain(llm, db)
+    #print(chain)
     response = chain.invoke({"question":prompt_text})
     out = fetch_data_synapse(serverless_connection_string, response)
     return response, out
-
+    #return response
